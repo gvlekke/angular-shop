58114c15e60d53d45f2648b9116e481c122ed20d
Glenn Vlekke <g.vlekke@sping.nl>
2021-03-23 21:33:05 +0100

    feat(routing): add lazy routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+export enum RoutesEnum {
+  HOME = '',
+}
 
+const routes: Routes = [
+  {
+    path: RoutesEnum.HOME,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./modules/home/home.module').then((m) => m.HomeModule),
+      },
+    ],
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
